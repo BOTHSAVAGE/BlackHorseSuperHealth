3031594d36323074c31b4574ae88630714ea20a2
BOTHSAVAGE <bothsavage@163.com>
2020-09-03 22:21:54 +0800

    检查组后端代码查阅：没有缓存，常规MVC

---
+++ health_parent/health_interface/src/main/java/com/bothsavage/service/CheckGroupService.java
@@ -0,0 +1,15 @@
+package com.bothsavage.service;
+
+import com.bothsavage.entity.PageResult;
+import com.bothsavage.entity.QueryPageBean;
+import com.bothsavage.pojo.CheckGroup;
+
+import java.util.List;
+
+public interface CheckGroupService {
+    public void add(CheckGroup checkGroup, Integer[] checkitemIds);
+    public PageResult pageQuery(QueryPageBean queryPageBean);
+    public CheckGroup findById(Integer id);
+    public List<Integer> findCheckItemIdsByCheckGroupId(Integer id);
+    public void edit(CheckGroup checkGroup, Integer[] checkitemIds);
+}
---
+++ health_parent/health_service_provider/src/main/java/com/bothsavage/dao/CheckGroupDao.java
@@ -0,0 +1,17 @@
+package com.bothsavage.dao;
+
+import com.bothsavage.pojo.CheckGroup;
+import com.github.pagehelper.Page;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CheckGroupDao {
+    public void add(CheckGroup checkGroup);
+    public void setCheckGroupAndCheckItem(Map map);
+    public Page<CheckGroup> findByCondition(String queryString);
+    public CheckGroup findById(Integer id);
+    public List<Integer> findCheckItemIdsByCheckGroupId(Integer id);
+    public void edit(CheckGroup checkGroup);
+    public void deleteAssocication(Integer id);
+}
